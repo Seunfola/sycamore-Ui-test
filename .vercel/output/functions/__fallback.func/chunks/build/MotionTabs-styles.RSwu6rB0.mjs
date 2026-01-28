2128bd7bf92b93c9555c1efd99801b159bd1d29b
seunfola <seunfola1@gmail.com>
2026-01-28 03:19:42 +0100

    fixing

---
+++ .vercel/output/functions/__fallback.func/chunks/build/MotionTabs-styles.RSwu6rB0.mjs
@@ -0,0 +1,8 @@
+const MotionTabs_vue_vue_type_style_index_0_scoped_771b78f1_lang = ".router-link-active[data-v-771b78f1]{color:#fff!important}nav>div[data-v-771b78f1]{pointer-events:none}";
+
+const MotionTabsStyles_RSwu6rB0 = [
+  MotionTabs_vue_vue_type_style_index_0_scoped_771b78f1_lang
+];
+
+export { MotionTabsStyles_RSwu6rB0 as default };
+//# sourceMappingURL=MotionTabs-styles.RSwu6rB0.mjs.map
